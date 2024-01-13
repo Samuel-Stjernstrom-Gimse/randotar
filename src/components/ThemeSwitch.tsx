2e1334978808c4901ac68ac046cefd878403b331
Zam <samuel.kodehode@gmail.com>
2024-01-13 19:28:23 +0100

    0.01

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+import {Switch, Tooltip} from "@mui/material";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSun} from "@fortawesome/free-solid-svg-icons/faSun";
+import {faMoon} from "@fortawesome/free-solid-svg-icons/faMoon";
+
+type Props = {
+    darkMode: boolean
+    toggleDarkMode: any
+}
+
+const ThemeSwitch = ({ darkMode, toggleDarkMode }: Props) => {
+
+    return (
+        <>
+            <Tooltip title={'Samuel er den dumme guttn'}>
+                <Switch
+                    checked={darkMode}
+                    onChange={toggleDarkMode}
+                    icon={<FontAwesomeIcon style={{ backgroundColor: '#095252', borderRadius: '50%', aspectRatio: '1/1', padding: '2px' }} icon={faSun} />}
+                    checkedIcon={<FontAwesomeIcon style={{ backgroundColor: '#595959', borderRadius: '50%', aspectRatio: '1/1', padding: '2px' }} icon={faMoon} />}
+                />
+            </Tooltip>
+        </>
+    )
+}
+
+export default ThemeSwitch
