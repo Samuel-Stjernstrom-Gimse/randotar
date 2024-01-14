980a3990d3f41de99dba3f8e3c5db381dede6f0f
Eivind <eivind.norling@tidsbanken.no>
2024-01-14 01:45:34 +0100

    feat: avatar generation and storage

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,12 +1,19 @@
-import {Box, Container} from "@mui/material";
+import { Box } from "@mui/material";
 import AvatarCreator from "./avatars/AvatarCreator";
 
 const MainSection = () => {
-  return (
-      <Box width={'100%'} height={'100%'} border={'2px solid red'}>
-          <AvatarCreator/>
-      </Box>
-  )
-}
+    return (
+        <Box
+            marginTop={'10vh'}
+            width={'100%'}
+            height={'100%'}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <AvatarCreator />
+        </Box>
+    );
+};
 
-export default MainSection
+export default MainSection;
